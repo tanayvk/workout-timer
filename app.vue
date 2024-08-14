8af292f2fdd20f488b3cb36e3f87aa48572e520f
Tanay Karnik <tanayvk@gmail.com>
2024-08-14 14:33:39 +0530

    feat(pwa): setup pwa using nuxt-pwa

--- app.vue
+++ app.vue
@@ -6,6 +6,7 @@ useSeoMeta({
 });
 </script>
 <template>
+  <NuxtPwaManifest />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/fonts"],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "@vite-pwa/nuxt"],
   fonts: {
     families: [
       { name: "Montserrat", provider: "google" },
@@ -10,4 +10,59 @@ export default defineNuxtConfig({
     ],
   },
   css: ["~/assets/main.css"],
+  nitro: {
+    routeRules: {
+      "/*": { prerender: true },
+    },
+  },
+  pwa: {
+    strategies: "generateSW",
+    registerWebManifestInRouteRules: true,
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Workout Timer",
+      short_name: "WorkoutTimer",
+      theme_color: "#57b9b9",
+      icons: [
+        {
+          src: "pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png",
+        },
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ["/", "**/*.{mjs,js,css,html,png,svg,ico}"],
+    },
+    injectManifest: {
+      globPatterns: ["/", "**/*.{mjs,js,css,html,png,svg,ico}"],
+    },
+    client: {
+      installPrompt: true,
+      // periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: "/",
+      navigateFallbackAllowlist: [/^\/$/],
+      type: "module",
+    },
+  },
 });
